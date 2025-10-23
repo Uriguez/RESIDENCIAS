8b364b80caae77d13e26af4360bbad776ed44909
uriguez <uri61220@gmail.com>
2025-10-22 23:54:08 -0600

    Nuevo

---
+++ SistemaCapacitacion.Core/Entities/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SistemaCapacitacion.Core.Entities
+{
+    public class User
+    {
+        public string Id { get; set; } // FK a AspNetUsers.Id 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Position { get; set; }
+        public string Department { get; set; }
+        public DateTime HireDate { get; set; }
+        public int Status { get; set; }
+        public DateTime? LastLoginDate { get; set; }
+        public string Avatar { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Propiedad de navegación para Identity (requerirá un paso extra)
+        // public virtual AspNetUser AspNetUser { get; set; } 
+    }
+}
