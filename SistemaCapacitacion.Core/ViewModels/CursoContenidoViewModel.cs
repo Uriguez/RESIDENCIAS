f6aef28f5edf2f650afe1ece98bd54df2eec00ee
Uriguez <uri61220@gmail.com>
2026-01-07 11:21:01 -0600

    Add TipoContenido property to CursoContenidoViewModel

--- SistemaCapacitacion.Core/ViewModels/CursoContenidoViewModel.cs
+++ SistemaCapacitacion.Core/ViewModels/CursoContenidoViewModel.cs
@@ -12,7 +12,8 @@ namespace SistemaCapacitacion.Core.ViewModels
         /// Ruta del archivo (video MP4 o documento PDF)
         /// </summary>
         public string RutaContenido { get; set; } = string.Empty;
-
+        
+        public int TipoContenido { get; set; } // 1=Video, 2=PDF, 3=Link
         /// <summary>
         /// true = Video | false = Documento
         /// </summary>
